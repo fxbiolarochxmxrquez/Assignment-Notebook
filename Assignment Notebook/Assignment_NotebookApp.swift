a13c21f2f8b01ee3ffc4f784ef50fde98e26c3a7
Fabiola Rocha Marquez <frochamarquez@bsd220.org>
2024-01-29 09:51:22 -0600

    Initial Commit

---
+++ Assignment Notebook/Assignment_NotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_NotebookApp.swift
+//  Assignment Notebook
+//
+//  Created by Fabiola Rocha Marquez on 1/29/24.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_NotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

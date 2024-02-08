c1b13796e08ee453cd3a2f8237504cad249eb76a
Fabiola Rocha Marquez <frochamarquez@bsd220.org>
2024-02-07 22:14:00 -0600

    created "addassignmentview"

---
+++ Assignment Notebook/AddAssignmentView.swift
@@ -0,0 +1,18 @@
+//
+//  AddAssignmentView.swift
+//  Assignment Notebook
+//
+//  Created by Fabiola Rocha Marquez on 2/7/24.
+//
+
+import SwiftUI
+
+struct AddAssignmentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddAssignmentView()
+}

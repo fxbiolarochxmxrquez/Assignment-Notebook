3c4babe1cc6afd391b676b59e26c225ab834083c
Fabiola Rocha Marquez <frochamarquez@bsd220.org>
2024-02-09 17:16:48 -0600

    enabled data persistance

--- Assignment Notebook/AssignmentList.swift
+++ Assignment Notebook/AssignmentList.swift
@@ -8,11 +8,22 @@
 import Foundation
 
 class AssignmentList: ObservableObject {
-    @Published var items =  [AssignmentItem(course: "Math", description: "Day #50", dueDate: Date()),
-                             AssignmentItem(course: "Mobile Apps", description: "assignment notebook", dueDate: Date()),
-                             AssignmentItem(course: "Accounting", description: "Chapter homework", dueDate: Date()),
-                             AssignmentItem(course: "English", description: "POTW", dueDate: Date()),
-                             AssignmentItem(course: "Psychology", description: "AP Classroom", dueDate: Date()),
-                             AssignmentItem(course: "Human Geography", description: "Key Issue 8.1", dueDate: Date()),
-                             AssignmentItem(course: "Physics", description: "Mastering Physics", dueDate: Date())]
+    @Published var items :  [AssignmentItem] {
+        didSet {
+            let encoder = JSONEncoder()
+            if let encoded = try? encoder.encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "data")
+            }
+        }
+    }
+    init() {
+        if let items = UserDefaults.standard.data(forKey: "data") {
+            let decoder = JSONDecoder()
+            if let decoded = try? decoder.decode([AssignmentItem].self, from: items) {
+                self.items = decoded
+                return
+            }
+        }
+        items = []
+    }
 }

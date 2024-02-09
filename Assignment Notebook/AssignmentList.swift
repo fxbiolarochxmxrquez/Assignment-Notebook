6e8a1f151ded4049fd5d2c1ba51c610d4d318f45
Fabiola Rocha Marquez <frochamarquez@bsd220.org>
2024-02-08 18:04:40 -0600

    completed "AddAssignmentView"

--- Assignment Notebook/AssignmentList.swift
+++ Assignment Notebook/AssignmentList.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+class AssignmentList: ObservableObject {
+    @Published var items =  [AssignmentItem(course: "Math", description: "Day #50", dueDate: Date()),
+                             AssignmentItem(course: "Mobile Apps", description: "assignment notebook", dueDate: Date()),
+                             AssignmentItem(course: "Accounting", description: "Chapter homework", dueDate: Date()),
+                             AssignmentItem(course: "English", description: "POTW", dueDate: Date()),
+                             AssignmentItem(course: "Psychology", description: "AP Classroom", dueDate: Date()),
+                             AssignmentItem(course: "Human Geography", description: "Key Issue 8.1", dueDate: Date()),
+                             AssignmentItem(course: "Physics", description: "Mastering Physics", dueDate: Date())]
+}
--- Assignment Notebook/ContentView.swift
+++ Assignment Notebook/ContentView.swift
@@ -8,17 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+   @ObservedObject var assignmentList = AssignmentList()
+    @State private var showingAddAssignmentView = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                ForEach(assignmentList.items) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.course)
+                                .font(.headline)
+                            Text(item.description)
+                        }
+                        Spacer()
+                        Text(item.dueDate, style: .date)
+                    }
+                }
+                .onMove { indices, newOffset in
+                    assignmentList.items.move(fromOffsets: indices, toOffset: newOffset)
+                }
+                .onDelete { indexSet in
+                    assignmentList.items.remove(atOffsets: indexSet)
+                }
+                .sheet(isPresented: $showingAddAssignmentView, content: {
+                    AddAssignmentView(assignmentList: assignmentList)
+                })
+                .navigationBarTitle("Assignment Notebook")
+                .navigationBarItems(leading: EditButton(),
+                                    trailing: Button(action: {
+                                        showingAddAssignmentView = true }) {
+                                        Image(systemName: "plus")
+                })
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct AssignmentItem: Identifiable {
+    var id = UUID()
+    var course = String()
+    var description = String()
+    var dueDate = Date()
 }
